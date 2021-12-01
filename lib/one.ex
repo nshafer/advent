e8fcb64cf39a8092f55857949555f7a22d647986
Nathan Shafer <nate@lotech.org>
2021-12-01 14:49:16 -0700

    Part 2

--- lib/one.ex
+++ lib/one.ex
@@ -1,4 +1,5 @@
 defmodule Advent.One do
+  # Part 1
   def count_depth_increases(depths) do
     depths
     |> Enum.reduce({nil, 0}, &determine_increase/2)
@@ -8,4 +9,12 @@ defmodule Advent.One do
   defp determine_increase(depth, {nil, acc}), do: {depth, acc}
   defp determine_increase(depth, {prev, acc}) when depth > prev, do: {depth, acc + 1}
   defp determine_increase(depth, {_prev, acc}), do: {depth, acc}
+
+  # Part 2
+  def count_depth_increases_by_window(depths) do
+    depths
+    |> Enum.chunk_every(3, 1)
+    |> Enum.map(&Enum.sum/1)
+    |> count_depth_increases()
+  end
 end
--- test/one_test.exs
+++ test/one_test.exs
@@ -1,9 +1,13 @@
 defmodule AdventOneTest do
   use ExUnit.Case
 
-  test "solution" do
+  test "part 1" do
     depths = Advent.load_ints("depths.txt")
-    increases = Advent.One.count_depth_increases(depths)
-    assert increases == 1462
+    assert Advent.One.count_depth_increases(depths) == 1462
+  end
+
+  test "part 2" do
+    depths = Advent.load_ints("depths.txt")
+    assert Advent.One.count_depth_increases_by_window(depths) == 1497
   end
 end
